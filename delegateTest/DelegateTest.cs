cbd24fcdd61ec8345fcbf0f6fa5b2abe4168d616
Jack Jiang <jiang1095@hotmail.com>
2015-05-25 01:13:59 +0800

    委托测试代码

---
+++ delegateTest/DelegateTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace delegateTest
+{
+    class DelegateTest
+    {
+        public delegate void run(string message);
+        private run runClient;
+        private string msg;
+        private Thread thread;
+
+        public DelegateTest(string msg, run runClient)
+        {
+            this.msg = msg;
+            this.runClient = runClient;
+            thread = new Thread(threadRun);
+            thread.Name = msg;
+        }
+
+        private void threadRun()
+        {
+            runClient(msg);
+        }
+
+        public void start()
+        {
+            thread.Start();
+        }
+    }
+}
---
+++ delegateTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateTest test = new DelegateTest("test", client);
+            test.start();
+            Console.ReadKey();
+        }
+
+        static void client(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
